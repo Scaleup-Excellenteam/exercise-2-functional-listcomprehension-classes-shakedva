21376feb32721231f9d081ada8fd311928c8a50c
shakedva <shakediii123@gmail.com>
2023-04-01 14:20:19 +0300

    added documentation

--- 6.3.long_cat_is_long.py
+++ 6.3.long_cat_is_long.py
@@ -1,11 +1,15 @@
 import re
 
+# non-alphanumeric characters regex
 PATTERN = r"([^\w\s])"
 
 
 def count_words(text):
+    """
+    :param text: str
+    :return: a dictionary that maps words from the text to their length
+    """
     text = text.lower()
     words = [re.sub(PATTERN, '', word) for word in text.split()]
     count_dict = {word: len(word) for word in words}
     return count_dict
-
