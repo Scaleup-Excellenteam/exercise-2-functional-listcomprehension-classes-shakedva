8dbb5e5f651d17d70e383783cfc84126922a51e6
shakedva <shakediii123@gmail.com>
2023-04-01 14:12:08 +0300

    added documentation

--- 6.2.2000run.py
+++ 6.2.2000run.py
@@ -2,13 +2,14 @@ import time
 
 
 def timer(f, *args, **kwargs):
+    """
+    :param f: function.
+    :param args: args to pass to the function.
+    :param kwargs: kwargs to pass to the function.
+    :return: The amount of time passed between starting the function received and ending it.
+    """
     start_time = time.time()
     f(args, **kwargs)
-    # time.sleep(2)
     end_time = time.time()
     return end_time - start_time
 
-
-# print(timer(print, "Hello"))
-# print(timer(zip, [1, 2, 3], [4, 5, 6]))
-# print(timer("Hi {name}".format, name="Bug"))
